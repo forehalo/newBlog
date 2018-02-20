a76582828ba511cb44fff9614e38035bae38072f
Igor Guastalla de Lima <limaguastallaigor@gmail.com>
2018-02-19 23:17:20 -0300

    Add pt-BR translation from en in the same format as the others.

---
+++ resources/lang/pt-BR/comment.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Comment Language Lines
+    |--------------------------------------------------------------------------
+    */
+    'fail' => 'Ops, falha ao comentar. Será corrigido em breve.',
+    'toggle_valid_fail' => 'Falha ao alternar a validação de comentário.',
+    'toggle_seen_fail' => 'Falha ao alternar o estado do comentário visto.',
+    'delete_fail' => 'Falha ao deletar o comentário.',
+
+];
